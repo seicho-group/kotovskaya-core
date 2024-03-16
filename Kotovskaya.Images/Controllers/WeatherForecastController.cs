46530d591f186094f229bcd7678ec150b90cc932
Konstaphy <Konstaphy@gmail.com>
2024-03-16 13:38:04 +0500

    поменял настройки для комфорта ежжи

--- Kotovskaya.Images/Controllers/WeatherForecastController.cs
+++ Kotovskaya.Images/Controllers/WeatherForecastController.cs
@@ -3,30 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace Kotovskaya.Images.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("/api/images")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    [HttpGet(Name = "GetWeatherForecast"), Route("")]
+    public String Get()
     {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return "Hello";
     }
 }
