0727326217782b610cae2e8c5b41c450166a7b70
Konstaphy <Konstaphy@gmail.com>
2024-05-18 03:09:45 +0500

    fix: убрал лишнюю депсу в гет продакт инфо

--- Kotovskaya.Products/Application/Services/GetProductInfo/GetProductInfoHandler.cs
+++ Kotovskaya.Products/Application/Services/GetProductInfo/GetProductInfoHandler.cs
@@ -1,5 +1,4 @@
-﻿using AspNetCore.Yandex.ObjectStorage;
-using AutoMapper;
+﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Confiti.MoySklad.Remap.Client;
 using Kotovskaya.DB.Domain.Context;
@@ -9,14 +8,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Kotovskaya.Products.Application.Services.GetProductInfo;
 
-public class GetProductInfoHandler(KotovskayaDbContext dbContext, IMapper mapper,YandexStorageService yandexStorageService)
+public class GetProductInfoHandler(KotovskayaDbContext dbContext, IMapper mapper)
     : IRequestHandler<GetProductInfoRequest, ProductEntityDto>
 {
     public async Task<ProductEntityDto> Handle(GetProductInfoRequest request, CancellationToken cancellationToken)
     {
-        var res = await yandexStorageService.ObjectService.GetAsync("photo_2024-05-01_23-24-49.jpg");
-
-        Console.WriteLine(res.StatusCode.ToString());
         var product = await dbContext.Products
             .ProjectTo<ProductEntityDto>(mapper.ConfigurationProvider)
             .FirstOrDefaultAsync(pr => pr.Id == request.ProductId, cancellationToken);
