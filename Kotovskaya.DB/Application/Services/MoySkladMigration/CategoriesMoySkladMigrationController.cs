b108add6f05610415efc7c07477fd68b3d63a621
Konstantine <Konstaphy@gmail.com>
2024-05-19 21:43:54 +0500

    fix: поправил миграцию категорий

--- Kotovskaya.DB/Application/Services/MoySkladMigration/CategoriesMoySkladMigrationController.cs
+++ Kotovskaya.DB/Application/Services/MoySkladMigration/CategoriesMoySkladMigrationController.cs
@@ -3,6 +3,7 @@ using Confiti.MoySklad.Remap.Entities;
 using Kotovskaya.DB.Application.Services.Interfaces;
 using Kotovskaya.DB.Domain.Context;
 using Kotovskaya.DB.Domain.Entities.DatabaseEntities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kotovskaya.DB.Application.Services.MoySkladMigration;
 
@@ -18,35 +19,37 @@ public class CategoriesMoySkladMigrationController : IMigrationController<Kotovs
         if (Api != null)
         {
             var categoriesResponse = await Api.ProductFolder.GetAllAsync();
-            MigrateCategoriesByPathName(categoriesResponse.Payload.Rows, "", null);
-            await KotovskayaDbContext.SaveChangesAsync();
+            await MigrateCategoriesByPathName(categoriesResponse.Payload.Rows, "", null);
+
         }
     }
 
-    private void MigrateCategoriesByPathName(ProductFolder[] categories, string pathName, Category? parentFolder)
+    private async Task MigrateCategoriesByPathName(ProductFolder[] categories, string pathName, Category? parentFolder)
     {
-        var childCategories = categories.Where(c => c.PathName.Split("/")[0] == pathName);
+        var childCategories = categories.Where(c => c.PathName.Split("/").Last() == pathName);
 
-        if (KotovskayaDbContext != null)
-            childCategories.ToList().ForEach(childCategory =>
+        if (KotovskayaDbContext == null) return;
+
+        foreach (var childCategory in childCategories.ToList())
+        {
+            var existingCategory = await KotovskayaDbContext.Categories
+                .FirstOrDefaultAsync(cat => cat.Id == childCategory.Id.ToString());
+
+            var newCategoryModel = new Category
             {
-                var existingCategory = KotovskayaDbContext.Categories
-                    .FirstOrDefault(cat => cat.Id == childCategory.Id.ToString());
+                ParentCategory = parentFolder,
+                ParentCategoryId = parentFolder?.Id,
+                Id = existingCategory?.Id ?? childCategory.Id.ToString() ?? Guid.NewGuid().ToString(),
+                Name = childCategory.Name,
+                Type = existingCategory?.Type ?? CategoryType.Soapmaking,
+                IsVisible = existingCategory?.IsVisible ?? true,
+                MsId = childCategory.Id.ToString()
+            };
 
-                var newCategoryModel = new Category
-                {
-                    ParentCategory = parentFolder,
-                    Id = childCategory.Id.ToString() ?? Guid.NewGuid().ToString(),
-                    Name = childCategory.Name,
-                    Type = existingCategory?.Type ?? CategoryType.Soapmaking,
-                    IsVisible = existingCategory?.IsVisible ?? true,
-                    MsId = childCategory.Id.ToString()
-                };
-
-                if (existingCategory != null) KotovskayaDbContext.Categories.Remove(existingCategory);
-                KotovskayaDbContext.Categories.Add(newCategoryModel);
-
-                MigrateCategoriesByPathName(categories, childCategory.Name, newCategoryModel);
-            });
+            if (existingCategory != null) KotovskayaDbContext.Categories.Remove(existingCategory);
+            await KotovskayaDbContext.Categories.AddAsync(newCategoryModel);
+            await KotovskayaDbContext.SaveChangesAsync();
+            await MigrateCategoriesByPathName(categories, childCategory.Name, newCategoryModel);
+        }
     }
 }
