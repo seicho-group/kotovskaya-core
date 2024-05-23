f2733608f01dd71d7733ef3ba24781dd48676c65
Konstantine <Konstaphy@gmail.com>
2024-05-23 21:27:52 +0500

    feat: проставляем сейлпрайс

--- Kotovskaya.DB/Application/Services/UpdatingDataController.cs/UpdatingDataController.cs
+++ Kotovskaya.DB/Application/Services/UpdatingDataController.cs/UpdatingDataController.cs
@@ -13,16 +13,30 @@ public class UpdatingDataController(KotovskayaMsContext msContext, KotovskayaDbC
             if (product?.MsId == null) continue;
 
             // if updated before last night, updating info
-            if (product.LastUpdatedAt < DateTime.Now.AddDays(-1))
+            if ((DateTime.Now - (product.LastUpdatedAt ?? DateTime.Now)).TotalDays <= 0)
             {
                 continue;
             }
             product.LastUpdatedAt = DateTime.Now.ToUniversalTime();
 
-            var productFromMoySklad = await msContext.FetchAssortmentInfoExtended(product.MsId.Value);
+            var productAssortmentFromMoySklad = await msContext.FetchAssortmentInfoExtended(product.MsId.Value);
+            var productFromMoySklad = await msContext.FetchProductInfoExtended(product.MsId.Value);
             if (productFromMoySklad == null) continue;
 
-            product.Quantity = productFromMoySklad.Quantity != null ? (int)productFromMoySklad.Quantity.Value : 0;
+            product.Quantity = productAssortmentFromMoySklad?.Quantity != null ?
+                (int)productAssortmentFromMoySklad.Quantity.Value
+                : 0;
+
+            var newSalePrice = new SaleTypes()
+            {
+                Product = product,
+                OldPrice = (int)(productFromMoySklad.SalePrices[2].Value ?? 0),
+                Price = (int)(productFromMoySklad.SalePrices[0].Value ?? 0)
+            };
+
+            var oldSalePrice = await dbContext.SaleTypes.Where(st => product.Id == st.Product.Id).FirstOrDefaultAsync();
+            if (oldSalePrice != null) dbContext.SaleTypes.Remove(oldSalePrice);
+            product.SaleTypes = newSalePrice;
         }
         await dbContext.SaveChangesAsync();
     }
