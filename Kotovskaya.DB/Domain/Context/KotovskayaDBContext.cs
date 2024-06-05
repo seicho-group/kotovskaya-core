1e6f3500ed4ef0b107c6d5fb1c80f90fc78c0c60
Konstantine <Konstaphy@gmail.com>
2024-06-05 23:45:56 +0500

    feat: добавил сингл обновление катгории

--- Kotovskaya.DB/Domain/Context/KotovskayaDBContext.cs
+++ Kotovskaya.DB/Domain/Context/KotovskayaDBContext.cs
@@ -26,7 +26,7 @@ public class KotovskayaDbContext : DbContext
         var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
         optionsBuilder.UseNpgsql($"" +
                                  $"Host={Environment.GetEnvironmentVariable("PG_HOST")};" +
-                                 $"Database={Environment.GetEnvironmentVariable(env == "development" ? "PG_DB_STAGE" : "PG_DB")};" +
+                                 $"Database={Environment.GetEnvironmentVariable("PG_DB")};" +
                                  $"Username=root;" +
                                  $"Password={Environment.GetEnvironmentVariable("PG_PASS")}");
     }
--- Kotovskaya.MoySkladUpdater/Program.cs
+++ Kotovskaya.MoySkladUpdater/Program.cs
@@ -15,9 +15,10 @@ var prUpdater = new ProductUpdater(msContext, dbContext, yaContext);
 var catUpdater = new CategoriesUpdater(msContext, dbContext, yaContext);
 var imgUpdater = new ImagesUpdater(msContext, dbContext, yaContext);
 
-Console.WriteLine("Categories updating...");
-await catUpdater.Migrate();
-Console.WriteLine("Products updating...");
-await prUpdater.Migrate();
-Console.WriteLine("Images updating...");
-await imgUpdater.Migrate();
+await prUpdater.MigrateSingleCategory(Guid.Parse("5af981ff-15a9-4e7f-acd1-eebc20e06ce5"));
+// Console.WriteLine("Categories updating...");
+// await catUpdater.Migrate();
+// Console.WriteLine("Products updating...");
+// await prUpdater.Migrate();
+// Console.WriteLine("Images updating...");
+// await imgUpdater.Migrate();
